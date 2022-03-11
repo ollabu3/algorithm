4b300666d588b385bb7be75bd3b87f05f881a4d7
ollabu3 <ollabu3@gmail.com>
2022-03-11 10:48:48 +0900

    study: 220315 크레인 인형뽑기 게임

---
+++ study/220315/crain.js
@@ -0,0 +1,45 @@
+//  node study/220315/crain.js
+
+function solution(board, moves) {
+  let answer = 0;
+  let stack = [];
+  let result = [];
+  let size = board.length;
+  let count = 0;
+
+  while (count !== size) {
+    let arr = [];
+    board.forEach((doll) => {
+      if (doll[count] !== 0) {
+        arr.push(doll[count]);
+      }
+    });
+    stack.push(arr);
+    count++;
+  }
+  for (let i = 0; i < moves.length; i++) {
+    if (stack[moves[i] - 1][0]) {
+      result.push(stack[moves[i] - 1][0]);
+      stack[moves[i] - 1].shift();
+      if (result[result.length - 1] === result[result.length - 2]) {
+        answer += 2;
+        result.splice(result.length - 2, 2);
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(
+  solution(
+    [
+      [0, 0, 0, 0, 0],
+      [0, 0, 1, 0, 3],
+      [0, 2, 5, 0, 1],
+      [4, 2, 4, 4, 2],
+      [3, 5, 1, 3, 1],
+    ],
+    [1, 5, 3, 5, 1, 2, 1, 4]
+  )
+);
