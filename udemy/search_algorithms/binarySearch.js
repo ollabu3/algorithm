1b447350a9872a36c73c391219400cb46454f91d
ollabu3 <ollabu3@gmail.com>
2022-06-02 17:40:58 +0900

    udemy: 검색 알고리즘

---
+++ udemy/search_algorithms/binarySearch.js
@@ -0,0 +1,49 @@
+function binarySearch(arr, value) {
+  let left = 0;
+  let middle = Math.floor(arr.length / 2);
+  let right = arr.length - 1;
+
+  while (arr[middle] !== value && left <= right) {
+    if (arr[middle] > value) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+    middle = Math.floor((left + right) / 2);
+  }
+  return arr[middle] === value ? middle : -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // 1
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // -1
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    10
+  )
+); // 2
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    95
+  )
+); // 16
+console.log(
+  binarySearch(
+    [
+      5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98,
+      99,
+    ],
+    100
+  )
+); // -1
+
+// node udemy/search_algorithms/binarySearch.js
